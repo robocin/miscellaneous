3d24d8a9b3bc9cab4fdea791c6a8b9cf8d2bd16d
Gabriel Bandeira <gmb@cin.ufpe.br>
2017-01-15 23:35:14 -0300

    Add script to install Atom and PlatformIO

---
+++ scripts/installAtom.sh
@@ -0,0 +1,5 @@
+wget http://platformio.org/get-started/ide?dl=platformio-atom-linux-x86_64.deb
+#echo "Check on the atom website (should open automaticaly) how to enable linter-clang"
+#xdg-open https://atom.io/packages/linter-clang
+sudo apt-get install -y clang
+apm install linter-clang
