1147b601dd21997d79940750ac4566b40d17e60c
Heitor Rapela <hrm@cin.ufpe.br>
2017-03-23 08:29:04 -0300

    Fix GStreamer Drive Erro. The repository of Gstreamer/ffmpeg was included in OtherLibs.sh

--- scripts/installOtherLibs.sh
+++ scripts/installOtherLibs.sh
@@ -1,2 +1,7 @@
 sudo apt-get install -y v4l-utils
 
+# Install GStreamer/ffmpeg (Opencv Lib to Video Stream)  
+# Adding repository manually to Ubuntu 14.04LTS
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update
+sudo apt-get install gstreamer0.10-ffmpeg
