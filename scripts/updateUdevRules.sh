fcf90487ddc4b43b5d181a338dfbff4a726ae376
Gabriel Bandeira <gmb@cin.ufpe.br>
2017-03-27 15:33:05 -0300

    Add script to create symbolic links to hardware on Ubuntu

---
+++ scripts/updateUdevRules.sh
@@ -0,0 +1,11 @@
+
+echo "sudo touch /etc/udev/rules.d/99-usb-serial.rules"
+sudo touch /etc/udev/rules.d/99-usb-serial.rules
+
+echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", ATTRS{serial}==\"AH01GYV4\", SYMLINK+=\"xBeeMaster\"" | sudo tee --append /etc/udev/rules.d/99-usb-serial.rules
+echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6015\", ATTRS{serial}==\"DN01E5ZP\", SYMLINK+=\"xBeeMaster\"" | sudo tee --append /etc/udev/rules.d/99-usb-serial.rules
+echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6015\", ATTRS{serial}==\"DN01E5ZT\", SYMLINK+=\"xBeeMaster\"" | sudo tee --append /etc/udev/rules.d/99-usb-serial.rules
+echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"10c4\", ATTRS{idProduct}==\"ea60\", ATTRS{serial}==\"00E59831\", SYMLINK+=\"robot\"" | sudo tee --append /etc/udev/rules.d/99-usb-serial.rules
+
+echo "sudo udevadm control --reload-rules"
+sudo udevadm control --reload-rules
