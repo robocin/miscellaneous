3747b5146a7923b61fc54ae872df0facec30b619
Raphael Brito <brito.raphaelc@gmail.com>
2017-10-30 15:14:28 -0300

    Update NRF ports

--- scripts/updateUdevRules.sh
+++ scripts/updateUdevRules.sh
@@ -2,6 +2,8 @@
 echo "sudo touch /etc/udev/rules.d/99-usb-serial.rules"
 sudo touch /etc/udev/rules.d/99-usb-serial.rules
 
+echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", ATTRS{serial}==\"AI05BAVI\", SYMLINK+=\"NRF_Master\"" | sudo tee --append /etc/udev/rules.d/99-usb-serial.rules
+echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", ATTRS{serial}==\"AI05BA5R\", SYMLINK+=\"NRF_Master\"" | sudo tee --append /etc/udev/rules.d/99-usb-serial.rules
 echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", ATTRS{serial}==\"AH01GYV4\", SYMLINK+=\"xBeeMaster\"" | sudo tee --append /etc/udev/rules.d/99-usb-serial.rules
 echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6015\", ATTRS{serial}==\"DN01E5ZP\", SYMLINK+=\"xBeeMaster\"" | sudo tee --append /etc/udev/rules.d/99-usb-serial.rules
 echo "SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6015\", ATTRS{serial}==\"DN01E5ZT\", SYMLINK+=\"xBeeMaster\"" | sudo tee --append /etc/udev/rules.d/99-usb-serial.rules
